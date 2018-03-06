b2821243f5f3321da2c00ecd3acd63b582593d3a
strehlst <strehl@szenar.io>
2018-03-06 00:52:08 +0000

    Add frontpage view and very basic spec to start reporting to Code Climate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def frontpage
+    SimpleMailer.simple_mail.deliver_now
+  end
 end
---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ApplicationController, type: :controller do
+  describe 'GET #index' do
+    it 'returns http success' do
+      get :frontpage
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
