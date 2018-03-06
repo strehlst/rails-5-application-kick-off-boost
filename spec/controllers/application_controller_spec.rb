cadde7f5522d14edc741a4770b1289d8fb8277c4
strehlst <strehl@szenar.io>
2018-03-06 03:43:40 +0000

    Spelling and new line cleanup

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
-  describe 'GET #index' do
+  describe 'GET #frontpage' do
     it 'returns http success' do
       get :frontpage
       expect(response).to have_http_status(:success)
--- spec/mailers/previews/simple_mailer_preview.rb
+++ spec/mailers/previews/simple_mailer_preview.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+# Preview all emails at http://localhost:3000/rails/mailers/simple_mailer
 class SimpleMailerPreview < ActionMailer::Preview
   def simple_mail
     SimpleMailer.simple_mail
